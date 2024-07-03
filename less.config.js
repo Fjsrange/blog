8f9b684a831b355f3e6c9c68e2efcd08c3ef0a4a
Orange <3434782480@qq.com>
2024-07-03 18:01:59 +0800

    首页页面框架搭建

---
+++ less.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // 是否使用浏览器自动转义 CSS 中的特殊字符
+  autoprefixer: {
+    disable: true,
+  },
+  // 是否将输入的 CSS 代码直接输出，而不是将其转换为 Less 代码
+  rawOutput: true,
+};
---
+++ src/components/routerButton/index.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>button</div>
+</template>
+
+<style lang="scss" scoped></style>
